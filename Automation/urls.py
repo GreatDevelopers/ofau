e72fcaa41cffbd15893f07275e8440863d97472e
sandy <sandy@kaur-sandeep.(none)>
2013-03-31 12:18:09 +0530

    urls.py file added

--- Automation/urls.py
+++ Automation/urls.py
@@ -1,29 +1,20 @@
-"""
-%% urls.py %%
-
-This file is define the main url for the Software. It links the software with different applications.
-"""
-
-#::::::::::::::IMPORT THE HEADER FILE HERE::::::::::::::::::::::::::::::#
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
-
-#:::::::::::::::DEFINE THE URLS HERE::::::::::::::::::::::::::::::::::::#
-
 
 urlpatterns = patterns('',
-   
-    (r'^$', direct_to_template,
-                    { 'template': 'index.html' },),
+    # Example:
+    (r'^$', 'Automation.tcc.views.index1'),
     (r'^hello', direct_to_template,
                     { 'template': 'job_ok.html' }, ),
-    (r'^tcc11_12/', include('Automation.tcc.urls')),
+    (r'^tcc/', include('Automation.tcc.urls')),
+    (r'^report/', include('Automation.report.urls')),
+    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
     (r'^accounts/', include('registration.urls')),
-   
    
 )
 
