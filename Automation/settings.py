116289608739f2eac44192ff963216f2a8a48b20
jasvir99 <jassigrewal91@gmail.com>
2013-07-14 23:26:26 +0530

    mpath added

--- Automation/settings.py
+++ Automation/settings.py
@@ -113,7 +113,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'Automation.urls'
 
-TEMPLATE_DIRS = ("/home/jass/Automation/templates"
+TEMPLATE_DIRS = ("mpath/Automation/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
