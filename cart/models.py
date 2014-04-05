5d182019b0b0ab73410507cc4da4ff1a8e3c4093
Jasvir Singh <jassigrewal91@gmail.com>
2014-04-05 10:46:44 +0530

    Initial database commit.

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+"""
+Models for cart module
+"""
+
+class category(models.Model):
+    parent = models.ForeignKey('self',null = True)
+    def __unicode__(self):
+        return self.id
+
+class attributeValue(models.Model):
+    attribute = models.ForeignKey(attributes)
+    value = models.CharField(max_length = 200)
+    product = models.ForeignKey(product)
+    def __unicode__(self):
+        return self.name
+
+class product(models.Model):
+    name = models.CharField(max_length = 100)
+    category = models.ForeignKey(category)
+    ammount = models.IntegerField()
+    def __unicode__(self):
+        return self.name
