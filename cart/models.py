32578d0cb2e7e51a937ea27e70e3114989f61815
Kamal <kamal>
2014-04-05 10:56:06 +0530

    Updating database

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,20 @@ from django.db import models
 """
 Models for cart module
 """
+class cart(models.Model):
+    user_id = models.CharField(max_length=11)
 
+class attributes(models.Model):
+    name = models.CharField(max_length=200)
+    is_number = models.BooleanField
+    is_string = models.BooleanField
+	def __unicode__(self):
+		return self.name
+
+class items(models.Model):
+    cost_id = models.ForeignKey(cart)
+    product_id = models.ForeignKey(product)
+	
 class category(models.Model):
     parent = models.ForeignKey('self',null = True)
     def __unicode__(self):
