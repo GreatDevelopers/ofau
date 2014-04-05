45f3a06c0111ec0bf3f3d452dcb30842bd87f880
Piyush <achyutapiyush@gmail.com>
2014-04-05 16:16:15 +0530

    Updated Database Schema

--- cart/models.py
+++ cart/models.py
@@ -1,17 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 """
 Models for cart module
 """
-class cart(models.Model):
-    user_id = models.CharField(max_length=11)
-
-class attributes(models.Model):
-    name = models.CharField(max_length=200)
-    is_number = models.BooleanField()
-    is_string = models.BooleanField()
-    def __unicode__(self):
-	return self.name
 
 class category(models.Model):
     parent = models.ForeignKey('self',null = True)
@@ -25,6 +17,13 @@ class product(models.Model):
     def __unicode__(self):
         return self.name
 
+class attributes(models.Model):
+    name = models.CharField(max_length=200)
+    is_number = models.BooleanField()
+    is_string = models.BooleanField()
+    def __unicode__(self):
+	   return self.name
+
 class items(models.Model):
     cost_id = models.ForeignKey(cart)
     product_id = models.ForeignKey(product)
@@ -36,3 +35,21 @@ class attributeValue(models.Model):
     def __unicode__(self):
         return self.name
 
+class purchase_order(models.Model):
+    """docstring for purchase_order"""
+    total = models.IntegerField()
+    buyer_id = models.ForeignKey(User)
+    is_debit = models.BooleanField()
+    def __init__(self, arg):
+        super(purchase_order, self).__init__()
+        self.arg = arg
+        
+class purchased_item(object):
+    """docstring for purchased_item"""
+    purchase_orderid = models.ForeignKey(purchase_order)
+    price = models.IntegerField()
+    item_id = models.ForeignKey(product)
+    def __init__(self, arg):
+        super(purchased_item, self).__init__()
+        self.arg = arg
+        
